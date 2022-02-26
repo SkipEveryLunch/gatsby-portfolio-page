6ace2327320da397a6374b2c1ce7be5e07dac72d
skipeverylunch <skipeverylunch@outlook.jp>
2022-02-26 19:05:38 +0900

    introduced tailwind css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,16 @@ import React from 'react'
 import {  Link } from 'gatsby'
 const Navbar = () =>{
 return (
-  <nav>
-  <h1>Cool log</h1>
-  <div className="links">
-    <Link to="/">Home</Link>
-    <Link to="/about">About Me</Link>
-    <Link to="/projects">Portfolios</Link>
-  </div>
-</nav>
+  <nav className="text-red-300 flex">
+    <div>
+      <h1>Cool log</h1>
+    </div>
+    <div className="links">
+      <Link to="/">Home</Link>
+      <Link to="/about">About Me</Link>
+      <Link to="/projects">Portfolios</Link>
+    </div>
+  </nav>
 )
   }
 export default Navbar
