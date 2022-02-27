d3feaaa4460a66b87ae5a9cae45f5af3f1720309
skipeverylunch <skipeverylunch@outlook.jp>
2022-02-27 12:23:43 +0900

    revised the layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ const Layout = ({children}) =>{
 return (
   <div className="h-screen flex flex-col">
     <Navbar />
-    <div className="h-full">
+    <div className="flex-1">
       { children }
     </div>
     <Footer />
