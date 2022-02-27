913ce15ef543459e5cdd3dc8bf666e93c025f364
skipeverylunch <skipeverylunch@outlook.jp>
2022-02-27 19:29:27 +0900

    redesined index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,25 @@
 import React from 'react'
 import Layout from "../components/Layout"
-import { Link,graphql } from 'gatsby'
-import { GatsbyImage } from "gatsby-plugin-image"
+import { Link } from 'gatsby'
 
-const Index = ({data}) =>{
+const Index = () =>{
 return (
   <Layout>
-    <section className="flex h-full">
-      <div className="flex-1 flex flex-col justify-center">
+    <section className="flex justify-center">
+      <div className="flex flex-col w-2/3 banner-section">
         <div className="text-4xl">
           <div className="mb-5">
-            <h2>このあたりに。</h2>
-            <h2>なんとなく。</h2>
-            <h2 >良い感じのポエムを。</h2>
+            <h2>Welcome.</h2>
+            <h2>I've got some portfolios</h2>
+            <h2 >that (I hope)
+              <span className="text-red-400 font-medium"> awe </span>
+             you.</h2>
           </div>
           <div className="text-base mb-5">
-            <p>この辺りに格好いいポエムが書いてあったら、ポートフォリオサイトとしてぐっと締まると思うのですが、残念ながら何を書けばいいのか見当もつきません。とりあえず下のポートフォリオ一覧でもご覧になってください。</p>
+            <p>SkipEveryLunchのポートフォリオサイトにようこそ。</p>
           </div>
         </div>
-        <Link className="inline-block w-max px-2 py-1 rounded-md bg-gray-700 text-white hover:bg-gray-500" to="/projects">ポートフォリオを見る</Link>
-      </div>
-      <div className="flex-1 flex flex-col justify-center">
-        <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} alt="banner"/>
+        <Link className="inline-block w-max px-2 py-1 rounded-md border-2 border-black hover:bg-gray-100" to="/projects">ポートフォリオを見る</Link>
       </div>
     </section>
   </Layout>
@@ -29,16 +27,3 @@ return (
   )
 }
 export default Index
-export const query = graphql`
-query Banner {
-  file(relativePath: {eq: "banner.png"}) {
-    childImageSharp {
-      gatsbyImageData(
-        layout: FULL_WIDTH
-        placeholder: BLURRED
-        formats: [AUTO, WEBP]
-        )
-    }
-  }
-}
-`
