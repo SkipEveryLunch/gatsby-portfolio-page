376d2781f61e419490a3167affba6c706650d097
skipeverylunch <skipeverylunch@outlook.jp>
2022-03-02 23:52:37 +0900

    added animation in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,16 @@ import { Link } from "gatsby"
 const Navbar = () => {
   return (
     <nav className="flex justify-center mx-4 border-b border-gray-300">
-      <Link className="py-2 mr-4" to="/">
+      <Link className="py-2 px-3 hover:bg-gray-100" to="/">
         Home
       </Link>
-      <Link className="py-2 mr-4" to="/about">
+      <Link className="py-2 px-3 hover:bg-gray-100" to="/about">
         About
       </Link>
-      <Link className="py-2 mr-4" to="/projects">
+      <Link className="py-2 px-3 hover:bg-gray-100" to="/projects">
         Portfolios
       </Link>
-      <Link className="py-2" to="/contact">
+      <Link className="py-2 px-2 hover:bg-gray-100" to="/contact">
         Contact
       </Link>
     </nav>
