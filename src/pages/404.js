16552294f1c5d47d65408f3ced78dd3de3c41879
skipeverylunch <skipeverylunch@outlook.jp>
2022-03-03 23:38:26 +0900

    implemented 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from "react"
+const NotFound = () => {
+  return (
+    <div className="h-full flex justify-center items-center">
+      <div>Page Not Found.</div>
+    </div>
+  )
+}
+export default NotFound
