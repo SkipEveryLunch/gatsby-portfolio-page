6dc69b6d129381925f6b9e00fdaaf1a43426570e
skipeverylunch <skipeverylunch@outlook.jp>
2022-03-03 22:26:02 +0900

    fixed a typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
-import React from 'react'
-const Footer = () =>{
-return (
-  <footer class="text-sm mt-auto mb-2 mr-2 text-right">
-    <p>Copyright © 2022 SkipEveryLunch</p>
-  </footer>
-)
-  }
+import React from "react"
+const Footer = () => {
+  return (
+    <footer className="text-sm mt-auto mb-2 mr-2 text-right">
+      <p>Copyright © 2022 SkipEveryLunch</p>
+    </footer>
+  )
+}
 export default Footer
