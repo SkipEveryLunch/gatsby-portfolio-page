5998534235f005a7d000c77d482c137316292b25
skipeverylunch <skipeverylunch@outlook.jp>
2022-03-05 11:20:41 +0900

    made NavLink Responsive

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 const NavLink = ({ isActive, name, path }) => {
   return (
     <Link
-      className={`py-2 px-3 hover:bg-gray-100 dark:hover:bg-gray-500 ${
+      className={`py-2 px-3 hover:bg-gray-100 dark:hover:bg-gray-500 text-sm md:text-base ${
         isActive ? "border-b-2 dark:border-gray-200 border-gray-400" : ""
       }`}
       to={path}
