8dd6c3335c1eebe2cd182e4d6ea8ae356dd8e028
skipeverylunch <skipeverylunch@outlook.jp>
2022-03-05 14:44:17 +0900

    implemented dark button in flex box way

--- src/components/DarkModeButton.js
+++ src/components/DarkModeButton.js
@@ -13,21 +13,20 @@ const DarkModeButton = () => {
     }
   }
   return (
-    <div className="flex items-center">
+    <div onClick={toggleIsDarkMode} className="flex items-center ">
       <div className="flex items-center">
         <FontAwesomeIcon
           className={`mr-1 ${isDarkMode ? "text-gray-300" : "text-blue-400"}`}
           icon={faSun}
         />
       </div>
-      <div className="flex items-center">
-        <input
-          onClick={toggleIsDarkMode}
-          className="darkModeButton"
-          type="checkbox"
-          role="switch"
-          id="flexSwitchCheckChecked"
-          checked={isDarkMode}
+      <div className="flex bg-gray-300 rounded-full w-10 h-4 p-0.5 relative">
+        <div
+          className={`duration-150 ${isDarkMode ? "flex-auto" : "flex-none"}`}
+        />
+        <div className="w-3 h-3 bg-white rounded-full"></div>
+        <div
+          className={`duration-150 ${isDarkMode ? "flex-none" : "flex-auto"}`}
         />
       </div>
       <div className="flex items-center">
